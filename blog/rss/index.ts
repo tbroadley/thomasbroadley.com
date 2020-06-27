6e0dc89df630c49ae4547a85af4a22fdb0f1d22e
Thomas Broadley <buriedunderbooks@hotmail.com>
2020-06-27 09:05:40 -0400

    Extract parser functions from Parser

--- blog/rss/index.ts
+++ blog/rss/index.ts
@@ -12,58 +12,60 @@ function parsePost(postBody) {
   let content = "";
   let inContent = false;
 
+  function onopentag(name, attributes) {
+    if (name === "title") {
+      inTitle = true;
+    }
+    if (name === "p" && attributes.class === "timestamp") {
+      inTimestamp = true;
+    }
+    if (name === "section") {
+      inContent = true;
+    } else if (inContent) {
+      const attributesString =
+        Object.keys(attributes).length === 0
+          ? ""
+          : " " +
+            Object.keys(attributes)
+              .map((key) => `${key}="${attributes[key]}"`)
+              .join(" ");
+      const closingBracket = SELF_CLOSING_TAG_NAMES.includes(name) ? "" : ">";
+      content += `<${name}${attributesString}${closingBracket}`;
+    }
+  }
+
+  function ontext(text) {
+    if (inTitle) {
+      title = text.replace(/—Thomas Broadley$/, "");
+    }
+    if (inTimestamp) {
+      timestamp = text.substring(0, "yyyy-mm-dd".length);
+    }
+    if (inContent) {
+      content += text;
+    }
+  }
+
+  function onclosetag(name) {
+    if (name === "title") {
+      inTitle = false;
+    }
+    if (name === "p") {
+      inTimestamp = false;
+    }
+    if (name === "section") {
+      inContent = false;
+    }
+    if (inContent) {
+      content += SELF_CLOSING_TAG_NAMES.includes(name) ? " />" : `</${name}>`;
+    }
+  }
+
   const parser = new Parser(
     {
-      onopentag(name, attributes) {
-        if (name === "title") {
-          inTitle = true;
-        }
-        if (name === "p" && attributes.class === "timestamp") {
-          inTimestamp = true;
-        }
-        if (name === "section") {
-          inContent = true;
-        } else if (inContent) {
-          const attributesString =
-            Object.keys(attributes).length === 0
-              ? ""
-              : " " +
-                Object.keys(attributes)
-                  .map((key) => `${key}="${attributes[key]}"`)
-                  .join(" ");
-          const closingBracket = SELF_CLOSING_TAG_NAMES.includes(name)
-            ? ""
-            : ">";
-          content += `<${name}${attributesString}${closingBracket}`;
-        }
-      },
-      ontext(text) {
-        if (inTitle) {
-          title = text.replace(/—Thomas Broadley$/, "");
-        }
-        if (inTimestamp) {
-          timestamp = text.substring(0, "yyyy-mm-dd".length);
-        }
-        if (inContent) {
-          content += text;
-        }
-      },
-      onclosetag(name) {
-        if (name === "title") {
-          inTitle = false;
-        }
-        if (name === "p") {
-          inTimestamp = false;
-        }
-        if (name === "section") {
-          inContent = false;
-        }
-        if (inContent) {
-          content += SELF_CLOSING_TAG_NAMES.includes(name)
-            ? " />"
-            : `</${name}>`;
-        }
-      },
+      onopentag,
+      ontext,
+      onclosetag,
     },
     { decodeEntities: true }
   );
