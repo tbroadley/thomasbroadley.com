394477f85a711d3325b28fe929c86485232bb342
Thomas Broadley <buriedunderbooks@hotmail.com>
2020-06-28 11:45:24 -0400

    Use getPostData in build-posts.ts

--- src/build-posts.ts
+++ src/build-posts.ts
@@ -2,27 +2,22 @@ import { copyFileSync, readFileSync, writeFileSync } from "fs";
 import * as globby from "globby";
 import { render } from "mustache";
 import { basename } from "path";
-import * as YAML from "yaml";
 
-const posts = globby
-  .sync("blog/*/*.yml")
-  .map((post) => post.replace(/^blog\//, ""))
-  .map((post) => post.replace(/\/index\.yml$/, ""));
+import { getPostData } from "./post-data";
 
+const posts = getPostData();
 const template = readFileSync("templates/post.html", "utf8");
 
-for (const post of posts) {
-  const dataYaml = readFileSync(`blog/${post}/index.yml`, "utf8");
-  const data = YAML.parse(dataYaml);
+for (const { path, ...data } of posts) {
   const renderedPost = render(template, data);
 
-  writeFileSync(`docs/blog/${post}/index.html`, renderedPost);
+  writeFileSync(`docs/blog/${path}/index.html`, renderedPost);
 
-  const files = globby.sync(`blog/${post}/*`);
+  const files = globby.sync(`blog/${path}/*`);
 
   for (const file of files) {
     if (file.endsWith("/index.yml")) continue;
 
-    copyFileSync(file, `docs/blog/${post}/${basename(file)}`);
+    copyFileSync(file, `docs/blog/${path}/${basename(file)}`);
   }
 }
