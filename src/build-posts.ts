31bdfb266060ed10eea041769615f9c28fd21daa
Thomas Broadley <buriedunderbooks@hotmail.com>
2020-06-28 10:55:18 -0400

    Add script to build posts

---
+++ src/build-posts.ts
@@ -0,0 +1,19 @@
+import { readFileSync, writeFileSync } from "fs";
+import * as globby from "globby";
+import { render } from "mustache";
+import * as YAML from "yaml";
+
+const posts = globby
+  .sync("blog/*.yml")
+  .map((post) => post.replace(/^blog\//, ""));
+
+const template = readFileSync("blog/template.html", "utf8");
+
+for (const post of posts) {
+  const dataYaml = readFileSync(`blog/${post}`, "utf8");
+  const data = YAML.parse(dataYaml);
+  const renderedPost = render(template, data);
+
+  const postFolder = post.replace(/\.yml$/, "");
+  writeFileSync(`docs/blog/${postFolder}/index.html`, renderedPost);
+}
