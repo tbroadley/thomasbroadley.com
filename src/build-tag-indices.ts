ff49fdaba36b6302cb256e860108638461884567
Thomas Broadley <buriedunderbooks@hotmail.com>
2020-09-20 11:45:42 -0400

    Add tag index

--- src/build-tag-indices.ts
+++ src/build-tag-indices.ts
@@ -6,10 +6,17 @@ import { render } from "./render";
 export function buildTagIndices() {
   const tagData = getTagData();
 
-  const template = readFileSync("templates/tag-post-list.html", "utf8");
+  const tagIndexTemplate = readFileSync("templates/tag-index.html", "utf8");
+  const renderedTagIndex = render(tagIndexTemplate, { tags: tagData });
+  writeFileSync(`docs/blog/tags/index.html`, renderedTagIndex);
+
+  const tagPostListTemplate = readFileSync(
+    "templates/tag-post-list.html",
+    "utf8"
+  );
 
   for (const tag of tagData) {
-    const renderedTagPostList = render(template, tag);
+    const renderedTagPostList = render(tagPostListTemplate, tag);
     writeFileSync(`docs/blog/tags/${tag.name}.html`, renderedTagPostList);
   }
 }
