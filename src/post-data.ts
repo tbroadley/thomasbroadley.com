7b3d1e5be79ff5d4b590cb5cf93cd8a6dba0782b
Thomas Broadley <buriedunderbooks@hotmail.com>
2020-09-20 10:13:15 -0400

    Add tags to posts

--- src/post-data.ts
+++ src/post-data.ts
@@ -3,14 +3,31 @@ import * as YAML from "yaml";
 import { orderBy } from "lodash";
 import * as globby from "globby";
 
-export function getPostData() {
+type PostData = {
+  path: string;
+  title: string;
+  titleHtml?: string;
+  description: string;
+  descriptionHtml?: string;
+  tags: string[];
+  tagsString: string;
+  body: string;
+  createdAt: string;
+  lastModified: string;
+};
+
+export function getPostData(): PostData[] {
   const posts = globby.sync("blog/posts/*/*.yml").map((post) => {
     const path = post
       .replace(/^blog\/posts\//, "")
       .replace(/\/index\.yml$/, "");
     const dataYaml = readFileSync(post, "utf8");
     const data = YAML.parse(dataYaml);
-    return { ...data, path };
+
+    const tags = data.tags ?? [];
+    const tagsString = tags.join(", ");
+
+    return { ...data, tags, tagsString, path };
   });
   return orderBy(posts, [({ createdAt }) => createdAt], ["desc"]);
 }
