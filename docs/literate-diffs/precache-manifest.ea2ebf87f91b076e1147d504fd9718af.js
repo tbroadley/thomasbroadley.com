ee6d3e8f6f93f394dd2fb1930ad859dd58f34b7e
Thomas Broadley <buriedunderbooks@hotmail.com>
2020-09-23 13:46:20 -0400

    Add Literate Diffs

---
+++ docs/literate-diffs/precache-manifest.ea2ebf87f91b076e1147d504fd9718af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "eb79970e9582873c31435f49057fe47f",
+    url: "/literate-diffs/index.html",
+  },
+  {
+    revision: "42680319bc9a10d0ab3d",
+    url: "/literate-diffs/static/css/2.3c515868.chunk.css",
+  },
+  {
+    revision: "f70665a8a94643945571",
+    url: "/literate-diffs/static/css/main.7bd37639.chunk.css",
+  },
+  {
+    revision: "42680319bc9a10d0ab3d",
+    url: "/literate-diffs/static/js/2.d944516b.chunk.js",
+  },
+  {
+    revision: "9598fde2a22ec3ce6af9cd5c173a9b46",
+    url: "/literate-diffs/static/js/2.d944516b.chunk.js.LICENSE.txt",
+  },
+  {
+    revision: "f70665a8a94643945571",
+    url: "/literate-diffs/static/js/main.aa355599.chunk.js",
+  },
+  {
+    revision: "a283f52044412aa79fc8",
+    url: "/literate-diffs/static/js/runtime-main.7f82ccdb.js",
+  },
+  {
+    revision: "e49f23b67a53743f28eecdf9c6a21550",
+    url: "/literate-diffs/static/media/move.e49f23b6.svg",
+  },
+]);
---
+++ docs/literate-diffs/static/js/main.aa355599.chunk.js
@@ -0,0 +1,390 @@
+(this["webpackJsonpliterate-diffs"] =
+  this["webpackJsonpliterate-diffs"] || []).push([
+  [0],
+  {
+    193: function (e, n, t) {},
+    194: function (e, n, t) {},
+    195: function (e, n, t) {},
+    196: function (e, n, t) {},
+    197: function (e, n, t) {
+      "use strict";
+      t.r(n);
+      var a = t(0),
+        c = t.n(a),
+        r = t(4),
+        o = t.n(r),
+        i = (t(73), t(35)),
+        l = t(36),
+        s = t(38),
+        f = t(37),
+        u = t(63),
+        d = t.n(u),
+        m = t(64),
+        h = t.n(m),
+        p = t(5),
+        v = t(67),
+        g = t(65),
+        E = t.n(g),
+        _ =
+          (t(192),
+          t(193),
+          function (e) {
+            var n = e.type,
+              t = e.ln2,
+              a = e.ln,
+              r = e.content;
+            return c.a.createElement(
+              "div",
+              {
+                className: "change "
+                  .concat("add" === n ? "change--added" : "", " ")
+                  .concat("del" === n ? "change--deleted" : ""),
+              },
+              c.a.createElement(
+                "div",
+                { className: "change__addition-or-deletion" },
+                "add" === n ? "+" : "",
+                "del" === n ? "-" : ""
+              ),
+              c.a.createElement(
+                "div",
+                { className: "change__line-number" },
+                "normal" === n ? t : a
+              ),
+              c.a.createElement(
+                "div",
+                { className: "change__content" },
+                "normal" === n ? r : r.slice(1)
+              )
+            );
+          }),
+        b =
+          (t(194),
+          function (e) {
+            var n = e.baseKey,
+              t = e.content,
+              a = e.changes;
+            return c.a.createElement(
+              "div",
+              { className: "chunk" },
+              c.a.createElement("p", { className: "chunk__content" }, t),
+              a.map(function (e) {
+                var t = e.type,
+                  a = e.ln1,
+                  r = e.ln2,
+                  o = e.ln,
+                  i = e.content,
+                  l = ""
+                    .concat(n, "-")
+                    .concat(t, "-")
+                    .concat("normal" === t ? "".concat(a, "-").concat(r) : o);
+                return c.a.createElement(
+                  _,
+                  Object.assign(
+                    { key: l, baseKey: l },
+                    { type: t, ln1: a, ln2: r, ln: o, content: i }
+                  )
+                );
+              })
+            );
+          }),
+        w = (t(195), t(66)),
+        k = t.n(w),
+        N = Object(p.SortableHandle)(function () {
+          return c.a.createElement("img", {
+            className: "file__drag-handle",
+            src: k.a,
+            alt: "Drag and drop this file",
+          });
+        }),
+        y = Object(p.SortableElement)(function (e) {
+          e.index;
+          var n,
+            t = e.from,
+            a = e.to,
+            r = e.chunks,
+            o = c.a.useState(""),
+            i = Object(v.a)(o, 2),
+            l = i[0],
+            s = i[1];
+          return (
+            (n =
+              t === a
+                ? c.a.createElement(
+                    "p",
+                    { className: "file__description" },
+                    c.a.createElement("span", { className: "file__name" }, t)
+                  )
+                : "/dev/null" === t
+                ? c.a.createElement(
+                    "p",
+                    { className: "file__description" },
+                    "File ",
+                    c.a.createElement("span", { className: "file__name" }, a),
+                    " created"
+                  )
+                : "/dev/null" === a
+                ? c.a.createElement(
+                    "p",
+                    { className: "file__description" },
+                    "File ",
+                    c.a.createElement("span", { className: "file__name" }, a),
+                    " deleted"
+                  )
+                : c.a.createElement(
+                    "p",
+                    { className: "file__description" },
+                    "File",
+                    c.a.createElement(
+                      "span",
+                      { className: "file__from-name" },
+                      t
+                    ),
+                    "renamed to",
+                    c.a.createElement("span", { className: "file__to-name" }, a)
+                  )),
+            c.a.createElement(
+              "div",
+              { className: "file" },
+              c.a.createElement(N, null),
+              c.a.createElement(
+                "div",
+                { className: "file__user-text" },
+                c.a.createElement(E.a, {
+                  value: l,
+                  onChange: function (e) {
+                    return s(e);
+                  },
+                })
+              ),
+              n,
+              "/dev/null" === a
+                ? null
+                : r.map(function (e) {
+                    var n = e.oldStart,
+                      r = e.newStart,
+                      o = e.content,
+                      i = e.changes,
+                      l = ""
+                        .concat(t, "-")
+                        .concat(a, "-")
+                        .concat(n, "-")
+                        .concat(r);
+                    return c.a.createElement(b, {
+                      key: l,
+                      baseKey: l,
+                      content: o,
+                      changes: i,
+                    });
+                  })
+            )
+          );
+        }),
+        j =
+          (t(196),
+          Object(p.SortableContainer)(function (e) {
+            var n = e.diff,
+              t = void 0 === n ? [] : n;
+            return c.a.createElement(
+              "div",
+              { className: "app" },
+              t.map(function (e, n) {
+                var t = e.from,
+                  a = e.to,
+                  r = e.chunks,
+                  o = e.chunkIndex;
+                return c.a.createElement(
+                  y,
+                  Object.assign(
+                    {
+                      key: "".concat(t, "-").concat(a, "-").concat(o),
+                      baseKey: "".concat(t, "-").concat(a, "-").concat(o),
+                    },
+                    { index: n, from: t, to: a, chunks: r }
+                  )
+                );
+              }),
+              c.a.createElement(
+                "p",
+                null,
+                "Icons made by ",
+                c.a.createElement(
+                  "a",
+                  {
+                    href: "https://www.flaticon.com/authors/freepik",
+                    title: "Freepik",
+                  },
+                  "Freepik"
+                )
+              )
+            );
+          })),
+        O = (function (e) {
+          Object(s.a)(t, e);
+          var n = Object(f.a)(t);
+          function t() {
+            var e;
+            Object(i.a)(this, t);
+            for (var a = arguments.length, c = new Array(a), r = 0; r < a; r++)
+              c[r] = arguments[r];
+            return (
+              ((e = n.call.apply(n, [this].concat(c))).state = { diff: "" }),
+              (e.onChange = function (n) {
+                e.setState({ diff: n.target.value });
+              }),
+              e
+            );
+          }
+          return (
+            Object(l.a)(t, [
+              {
+                key: "render",
+                value: function () {
+                  var e = this;
+                  return c.a.createElement(
+                    "div",
+                    { className: "app" },
+                    c.a.createElement("p", null, "Paste in a Git diff:"),
+                    c.a.createElement(
+                      "p",
+                      null,
+                      c.a.createElement("textarea", {
+                        value: this.state.diff,
+                        onChange: this.onChange,
+                      })
+                    ),
+                    c.a.createElement(
+                      "button",
+                      {
+                        onClick: function () {
+                          return e.props.setDiff(e.state.diff);
+                        },
+                      },
+                      "Lit that diff!"
+                    )
+                  );
+                },
+              },
+            ]),
+            t
+          );
+        })(a.Component),
+        S = (function (e) {
+          Object(s.a)(t, e);
+          var n = Object(f.a)(t);
+          function t() {
+            var e;
+            Object(i.a)(this, t);
+            for (var a = arguments.length, c = new Array(a), r = 0; r < a; r++)
+              c[r] = arguments[r];
+            return (
+              ((e = n.call.apply(n, [this].concat(c))).state = {}),
+              (e.onSortEnd = function (n) {
+                var t = n.oldIndex,
+                  a = n.newIndex;
+                e.setState({ diff: Object(p.arrayMove)(e.state.diff, t, a) });
+              }),
+              (e.setDiff = function (n) {
+                var t = h()(n),
+                  a = d()(t, function (e) {
+                    var n = e.from,
+                      t = e.to;
+                    return e.chunks.map(function (e, a) {
+                      return { from: n, to: t, chunks: [e], chunkIndex: a };
+                    });
+                  });
+                e.setState({ diff: a });
+              }),
+              e
+            );
+          }
+          return (
+            Object(l.a)(t, [
+              {
+                key: "render",
+                value: function () {
+                  var e = this.state.diff;
+                  return e
+                    ? c.a.createElement(j, {
+                        diff: e,
+                        onSortEnd: this.onSortEnd,
+                        useDragHandle: !0,
+                      })
+                    : c.a.createElement(O, { setDiff: this.setDiff });
+                },
+              },
+            ]),
+            t
+          );
+        })(a.Component),
+        x = Boolean(
+          "localhost" === window.location.hostname ||
+            "[::1]" === window.location.hostname ||
+            window.location.hostname.match(
+              /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+            )
+        );
+      function C(e) {
+        navigator.serviceWorker
+          .register(e)
+          .then(function (e) {
+            e.onupdatefound = function () {
+              var n = e.installing;
+              n.onstatechange = function () {
+                "installed" === n.state &&
+                  (navigator.serviceWorker.controller
+                    ? console.log("New content is available; please refresh.")
+                    : console.log("Content is cached for offline use."));
+              };
+            };
+          })
+          .catch(function (e) {
+            console.error("Error during service worker registration:", e);
+          });
+      }
+      o.a.render(c.a.createElement(S, null), document.getElementById("root")),
+        (function () {
+          if ("serviceWorker" in navigator) {
+            if (
+              new URL("/literate-diffs", window.location).origin !==
+              window.location.origin
+            )
+              return;
+            window.addEventListener("load", function () {
+              var e = "".concat("/literate-diffs", "/service-worker.js");
+              x
+                ? (function (e) {
+                    fetch(e)
+                      .then(function (n) {
+                        404 === n.status ||
+                        -1 ===
+                          n.headers.get("content-type").indexOf("javascript")
+                          ? navigator.serviceWorker.ready.then(function (e) {
+                              e.unregister().then(function () {
+                                window.location.reload();
+                              });
+                            })
+                          : C(e);
+                      })
+                      .catch(function () {
+                        console.log(
+                          "No internet connection found. App is running in offline mode."
+                        );
+                      });
+                  })(e)
+                : C(e);
+            });
+          }
+        })();
+    },
+    66: function (e, n, t) {
+      e.exports = t.p + "static/media/move.e49f23b6.svg";
+    },
+    68: function (e, n, t) {
+      e.exports = t(197);
+    },
+    73: function (e, n, t) {},
+  },
+  [[68, 1, 2]],
+]);
+//# sourceMappingURL=main.aa355599.chunk.js.map
