40a0d7c1945178a4aa4cc73fb45095c544c1ec27
Thomas Broadley <buriedunderbooks@hotmail.com>
2020-09-27 08:53:24 -0400

    Add custom 404 page

--- src/build-basic-pages.ts
+++ src/build-basic-pages.ts
@@ -2,9 +2,14 @@ import { readFileSync, writeFileSync } from "fs";
 
 import { render } from "./render";
 
-export function buildIndex() {
+export function buildBasicPages() {
   const template = readFileSync("templates/index.html", "utf8");
   const renderedIndex = render(template, {});
 
   writeFileSync(`docs/index.html`, renderedIndex);
+
+  const template404 = readFileSync("templates/404.html", "utf8");
+  const rendered404 = render(template404, {});
+
+  writeFileSync(`docs/404.html`, rendered404);
 }
--- src/build.ts
+++ src/build.ts
@@ -1,10 +1,10 @@
 import { buildBlogIndex } from "./build-blog-index";
-import { buildIndex } from "./build-index";
+import { buildBasicPages } from "./build-basic-pages";
 import { buildPosts } from "./build-posts";
 import { buildRss } from "./build-rss";
 import { buildTagIndices } from "./build-tag-indices";
 
-buildIndex();
+buildBasicPages();
 buildPosts();
 buildBlogIndex();
 buildTagIndices();
