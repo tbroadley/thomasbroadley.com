3852376e344fd2c1a09b5e19cabfad9f1c78b145
Thomas Broadley <buriedunderbooks@hotmail.com>
2022-08-27 07:54:32 -0400

    Make OG images larger

--- convert-images.zsh
+++ convert-images.zsh
@@ -12,7 +12,7 @@ for file in blog/**/*.jpg; do cp $file $file.original; done
 mogrify -format jpg -resize 64x64 blog/**/*.png
 for file in blog/**/*.jpg; do mv $file $(echo $file | sed s/.jpg//).jpg.thumbnail; done
 
-mogrify -format jpg -resize 256x256 blog/**/*.png
+mogrify -format jpg -resize 512x512 blog/**/*.png
 for file in blog/**/*.jpg; do mv $file $(echo $file | sed s/.jpg//).jpg.og; done
 
 for file in blog/**/*.jpg.original; do mv $file $(echo $file | sed s/.original//); done
